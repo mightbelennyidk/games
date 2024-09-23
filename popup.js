92f099028e9b7ba46fa4ba8317dc46b0e01e25f8
mightbelennyidk <hughjassapr1@gmail.com>
2024-09-22 19:24:58 -0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,5 @@
+popup.js.
+document.getElementById('reload-button').addEventListener('click', () => {
+    const iframe = document.getElementById('roblox-frame');
+    iframe.src = iframe.src; 
+});
